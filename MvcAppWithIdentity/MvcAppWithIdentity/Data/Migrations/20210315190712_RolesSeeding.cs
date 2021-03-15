b589644107ebe9ca893f1c4415fe24cfedb3d850
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-03-15 23:30:52 +0200

    MVC app with identity

---
+++ MvcAppWithIdentity/MvcAppWithIdentity/Data/Migrations/20210315190712_RolesSeeding.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcAppWithIdentity.Data.Migrations
+{
+    public partial class RolesSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddRole("admin");
+            migrationBuilder.AddRole("back-office");
+
+            migrationBuilder.AddUserWithRoles(
+                email: "admin@fasttrackit.ro",
+                password: "Admin123!",
+                new[] { "admin" });
+
+            migrationBuilder.AddUserWithRoles(
+                email: "backoffice@fasttrackit.ro",
+                password: "Back123!",
+                new[] { "back-office" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
